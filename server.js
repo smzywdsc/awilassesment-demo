e9a7d395b405598af1f620c345b38e6a78f682d6
smzywdsc <82203503+smzywdsc@users.noreply.github.com>
2025-06-20 17:37:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 数据库连接配置
 const db = mysql.createConnection({
-  host: 'localhost',
-  port: 3306, 
-  user: 'root',
-  password: '12345Wxz',
-  database: 'tool_set',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   charset: 'utf8mb4'
 });
 
